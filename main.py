441e495677766685b33e7b69c21932371eb4e626
Liu Min <liuminish@gmail.com>
2026-04-21 15:05:33 +0800

    update to quote sender

--- main.py
+++ main.py
@@ -13,26 +13,32 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
+SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD")
+# RECEIPIENT_EMAIL = os.environ.get("RECEIPIENT_EMAIL")
+RECEIPIENT_EMAIL = "liuminish@gmail.com"
+DAYS = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday"
+}
 
 today = datetime.now()
-today_tuple = (today.month, today.day)
+weekday = DAYS[today.weekday()]
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+with open('quotes.txt', 'r') as f:
+    quotes = f.readlines()
+body = random.choice(quotes)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    connection.starttls()
+    connection.login(SENDER_EMAIL, SENDER_PASSWORD)
+    connection.sendmail(
+        from_addr=SENDER_EMAIL,
+        to_addrs=RECEIPIENT_EMAIL,
+        msg=f"Subject:Here's some {weekday} motivation!\n\n{body}"
+    )
